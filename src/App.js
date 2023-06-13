18432a3c1b5383b0983ba529038971809c3b6d98
Lisa S <halisasaipulla@gmail.com>
2023-06-13 10:46:00 -0700

    implemented scroll places feature

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
+
     const [coordinates, setCoordinates] = useState({lat:0, lng:0});
     const [bounds, setBounds] = useState({});
+
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -18,11 +22,13 @@ const App = () => {
     }, []);
 
     useEffect(() => {
+        setIsLoading(true);
         console.log(coordinates, bounds);
         getPlaceData(bounds.sw, bounds.ne)
             .then((data)=>{
                 console.log(data);
                 setPlaces(data);
+                setIsLoading(false);
             })
     }, [coordinates, bounds]);
 
@@ -32,7 +38,11 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List places={places}/>
+                    <List 
+                        places={places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
@@ -40,6 +50,7 @@ const App = () => {
                         setBounds={setBounds}
                         coordinates={coordinates}
                         places={places}
+                        setChildClicked={setChildClicked}
                     />
                 </Grid>
             </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
@@ -6,7 +6,7 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = ({ setCoordinates, setBounds, coordinates, places}) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked}) => {
     console.log(places)
     const classes = useStyles();
     const isMobile = useMediaQuery('min-width:600px');
@@ -25,7 +25,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places}) => {
                     setCoordinates({ lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
-                onChildClick={''}
+                onChildClick={(child) => setChildClicked(child)}
             >
                 {places.length && places.map((place, i) => (
                     <div
